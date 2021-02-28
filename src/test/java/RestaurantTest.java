d75989baf5f3afa3b3c904baa2fdc8d6a4dbf2a8
talwinder1306 <46379206+talwinder1306@users.noreply.github.com>
2021-02-28 16:48:33 +0530

    Part 2: Solution - update test input

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -5,8 +5,6 @@ import org.mockito.Mockito;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,44 +30,31 @@ class RestaurantTest {
     /**
      * This method uses a partial mocked restaurant object since we need to check the values present in the object,
      * to determine if the current time is between open and closing time.
-     * Here, we are generating a Random time between the restaurant's opening and closing time.
-     * We mock the call to getCurrentTime() to return this random number between the restaurant's opening and closing time.
+     * Here, we are passing a time between the restaurant's opening and closing time.
+     * We mock the call to getCurrentTime() to return this time between the restaurant's opening and closing time.
      * And we expect the isRestaurantOpen() method to return true
      */
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
         Restaurant mockRestaurant = Mockito.spy(restaurant);
-        LocalTime betweenOpenAndClose = getRandomTimeBetween(mockRestaurant.openingTime, mockRestaurant.closingTime);
+        LocalTime betweenOpenAndClose = LocalTime.parse("13:00:00");
 
         Mockito.when(mockRestaurant.getCurrentTime()).thenReturn(betweenOpenAndClose);
         assertTrue(mockRestaurant.isRestaurantOpen());
     }
 
-    /**
-     *
-     * @param lowerBound - the lower bound of time range
-     * @param upperBound - the upper bound of time range
-     * @return Random time between lower and upper bound
-     */
-    private LocalTime getRandomTimeBetween(LocalTime lowerBound, LocalTime upperBound) {
-        return LocalTime.ofSecondOfDay(ThreadLocalRandom
-                .current()
-                .nextInt(lowerBound.toSecondOfDay(), upperBound.toSecondOfDay()));
-    }
+
     /**
      * This method uses a partial mocked restaurant object since we need to check the values present in the object,
      * to determine if the current time is outside open and closing time.
-     * Here, we are generating a Random time between the restaurant's closing and opening time i.e. the outside hours.
-     * We mock the call to getCurrentTime() to return this random number between the restaurant's outside hours.
+     * Here, we are passing a time between the restaurant's closing and opening time i.e. the outside hours.
+     * We mock the call to getCurrentTime() to return this time between the restaurant's outside hours.
      * And we expect the isRestaurantOpen() method to return false
      */
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         Restaurant mockRestaurant = Mockito.spy(restaurant);
-
-        LocalTime betweenMidnightAndOpen = getRandomTimeBetween(LocalTime.MIDNIGHT, mockRestaurant.openingTime);
-        LocalTime betweenCloseAndMidnight = getRandomTimeBetween(mockRestaurant.closingTime, LocalTime.parse("23:59:59"));
-        LocalTime betweenCloseAndOpen = Math.random() < 0.5 ? betweenMidnightAndOpen : betweenCloseAndMidnight;
+        LocalTime betweenCloseAndOpen = LocalTime.parse("23:05:00");
 
         Mockito.when(mockRestaurant.getCurrentTime()).thenReturn(betweenCloseAndOpen);
 
@@ -121,7 +106,7 @@ class RestaurantTest {
         List<String> itemNames = new ArrayList<>();
         itemNames.add("Sweet corn soup");
         itemNames.add("Vegetable lasagne");
-        int expectedOrderTotal = restaurant.getMenu().get(0).getPrice() + restaurant.getMenu().get(1).getPrice();
+        int expectedOrderTotal = 388;
         assertEquals(expectedOrderTotal, restaurant.getOrderTotal(itemNames));
     }
 
